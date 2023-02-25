77f415bbac1371752e1cfb3bb7e55841584045e7
thienlao999 <romannovikov1337@mail.ru>
2023-02-25 22:01:37 +0300

    HW DONE

--- homework(including traffic light)/ViewController.swift
+++ homework(including traffic light)/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = 1
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        
+        
     }
+    @IBAction func pressingChangeColorButton() {
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            changeColorButton.setTitle("Next!", for: .normal)
+        } else if
+            yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.3
+            redView.alpha = 1
 
-
+        }
+    }
 }
 
